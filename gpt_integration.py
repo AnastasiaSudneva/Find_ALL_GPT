9e75d8831a43cb1ea99b3ec3000be856da9a91ea
Vit-sa07 <sevav1507@gmail.com>
2024-01-14 21:44:44 +0300

    comment

--- gpt_integration.py
+++ gpt_integration.py
@@ -1,19 +1,22 @@
-import g4f
+import g4f  # Импорт модуля 'g4f'
 
-from message_handler import prompt_for_gpt
+from message_handler import prompt_for_gpt  # Импорт 'prompt_for_gpt' из модуля 'message_handler'
 
 
-def get_gpt_response(message, switch=False):
-    try:
-        prompt = prompt_for_gpt + message
-        if switch:
-            prompt += "\nПожалуйста, дайте другой ответ на этот же вопрос."
+def get_gpt_response(message,
+                     switch=False):  # Определение функции 'get_gpt_response' с параметрами 'message' и 'switch' (по умолчанию False).
+    try:  # Начало блока try для перехвата исключений, которые могут возникнуть внутри блока.
+        prompt = prompt_for_gpt + message  # Конкатенация 'prompt_for_gpt' с аргументом 'message' для формирования полной подсказки для GPT. Здесь мы указываем боту его область и сам запрос пользователя
+        if switch:  # Проверка, равен ли аргумент 'switch' значению True.
+            prompt += "\nПожалуйста, дайте другой ответ на этот же вопрос."  # Если 'switch' равен True, добавляется предложение с просьбой дать другой ответ на тот же вопрос. Когда нажимаем кнопку другой ответ
 
-        response = g4f.ChatCompletion.create(
-            model=g4f.models.default,
+        response = g4f.ChatCompletion.create(  # Создание запроса к gpt.
+            model=g4f.models.default,  # Указание на использование стандартной модели из 'g4f.models'.
             messages=[{"role": "user", "content": prompt}],
-            timeout=120,  # in secs
+            # Выбираем моделб юзер, в content передается содержание запроса
+            timeout=120,  # Установка времени ожидания ответа от GPT в 120 секунд.
         )
-        return response
-    except Exception as e:
-        return f"Ошибка: {e}"
+        return response  # Возврат полученного от GPT ответа.
+
+    except Exception as e:  # Перехват любых исключений, возникших в блоке try.
+        return f"Ошибка: {e}"  # Возврат сообщения об ошибке с деталями исключения.
